d3a33b1436ae58368c497cbaedca64730c183b86
JoseUrtubey <joseaurtubey@gmail.com>
2022-03-25 10:11:44 -0300

    ejercicio 6 resuelto

---
+++ ejercicio 6.py
@@ -0,0 +1,10 @@
+def contar_vocales(cadena):
+	contador = 0
+	for letra in cadena:
+		if letra.lower() in "aeiou":
+			contador += 1
+	return contador
+
+cadena = str(input("ingrese una frase para ver cuantas vocales tiene"))
+cantidad = contar_vocales(cadena)
+print(f"En la cadena '{cadena}'' hay {cantidad} vocales")
