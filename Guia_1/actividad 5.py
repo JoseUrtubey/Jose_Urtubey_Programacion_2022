32250e5fcce0a0c6f19dd035a0bf3984362d5d9d
JoseUrtubey <joseaurtubey@gmail.com>
2022-04-01 08:58:44 -0300

    ejercicios formateados

--- Guia_1/actividad 5.py
+++
@@ -1,8 +0,0 @@
-anchura = int(input("Anchura del rectángulo: "))
-altura = int(input("Altura del rectángulo: "))
-dibujo = str(input("ingrese el caracter con el cual hacer el dibujo:"))
-
-for i in range(altura):
-    for j in range(anchura):
-        print(dibujo, end="")
-    print()
--- ejercicio 6.py
+++ ejercicio 6.py
@@ -1,9 +1,10 @@
 def contar_vocales(cadena):
-	contador = 0
-	for letra in cadena:
-		if letra.lower() in "aeiou":
-			contador += 1
-	return contador
+    contador = 0
+    for letra in cadena:
+        if letra.lower() in "aeiou":
+            contador += 1
+    return contador
+
 
 cadena = str(input("ingrese una frase para ver cuantas vocales tiene"))
 cantidad = contar_vocales(cadena)
